8cc4e39acb2660f0c907dcdbae89121be4f4b580
SURYA <suryarajeswaran0901@gmail.com>
2026-08-02 11:51:15 +0530

    Change output declaration and update case format

--- FULL_SUBTRACTOR/full_subbehavioral.v
+++ FULL_SUBTRACTOR/full_subbehavioral.v
@@ -1,17 +1,17 @@
 module full_subbeha (a,b,c,diff,bor);
 input a,b,c;
-output diff,bor;
+output reg diff,bor;
 always @(a,b,c)
 begin 
     case ({a,b,c})
-    3'000:begin diff=0;bor=0;end
-    3'001:begin diff=1;bor=1;end
-    3'010:begin diff=1;bor=0;end
-    3'011:begin diff=0;bor=1;end
-    3'100:begin diff=1;bor=0;end
-    3'101:begin diff=0;bor=1;end
-    3'110:begin diff=0;bor=0;end
-    3'111:begin diff=1;bor=0;end
+    3'b000:begin diff=0;bor=0;end
+    3'b001:begin diff=1;bor=1;end
+    3'b010:begin diff=1;bor=0;end
+    3'b011:begin diff=0;bor=1;end
+    3'b100:begin diff=1;bor=0;end
+    3'b101:begin diff=0;bor=1;end
+    3'b110:begin diff=0;bor=0;end
+    3'b111:begin diff=1;bor=0;end
     endcase
 end
 endmodule
